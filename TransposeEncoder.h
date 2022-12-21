76ebf330dbcfb9305244c14fb1f02e7a5b50ac64
Vladimir10001 <104554028+Vladimir10001@users.noreply.github.com>
2022-12-20 23:22:44 -0800

    Create TransposeEncoder.h

---
+++ TransposeEncoder.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Helper.h"
+#include "Encoder.h"
+#include <iostream>
+
+class TransposeEncoder : public Encoder
+{
+public:
+    int m;
+    int n;
+    TransposeEncoder(int m, int n);
+    string encode(string& text, int& M, int& N);
+};
